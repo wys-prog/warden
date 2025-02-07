f5f220be32a5bda80f3d03be0a93b9e7c59a82e1
Hüseyin ÖZTÜRK <wys-workshop@outlook.fr>
2025-02-07 13:23:18 +0100

    adding support of local warden file scan.

--- src/warden.cpp
+++ src/warden.cpp
@@ -1,3 +1,4 @@
+#include <filesystem>
 #include <iostream>
 #include <sstream>
 #include <fstream>
@@ -10,36 +11,72 @@
 #include "compiler.hpp"
 #include "nodes.hpp"
 
+namespace fs = std::filesystem;
 
-int main(int argc, char *const argv[]) {
+std::vector<std::string> get_files(const std::string &directory = "./")
+{
+  std::vector<std::string> files;
+  try
+  {
+    for (const auto &entry : fs::directory_iterator(directory))
+    {
+      if (entry.is_regular_file())
+      {
+        std::string extension = entry.path().extension().string();
+        if (extension == ".wa" || extension == ".warden")
+        {
+          files.push_back(entry.path().string());
+          std::cout << "file found: " << entry.path().string() << std::endl;
+        }
+      }
+    }
+  }
+  catch (const fs::filesystem_error &e)
+  {
+    std::cerr << "Error accessing directory: " << e.what() << '\n';
+  }
+  return files;
+}
+
+int main(int argc, char *const argv[])
+{
   std::cout << "Warden 1.0\n";
 
   std::vector<std::string> instructions, inputs;
   std::vector<wylma::warden::node> nodes;
 
-  for (int i = 1; i < argc; i++) {
-    if (std::string(argv[i]) == "-f") {
+  for (int i = 1; i < argc; i++)
+  {
+    if (std::string(argv[i]) == "-f")
+    {
       inputs.push_back(std::string(argv[++i]));
-    } else {
+    }
+    else
+    {
       instructions.push_back(argv[i]);
     }
   }
 
-  for (size_t i = 0; i < instructions.size(); i++) {
-    wylma::warden::node n;
+  // Get all .wa and .warden files
+  std::vector<std::string> found_files = get_files();
+  inputs.insert(inputs.end(), found_files.begin(), found_files.end());
 
+  for (size_t i = 0; i < instructions.size(); i++)
+  {
+    wylma::warden::node n;
     n.id = "main@" + std::to_string(i);
     n.ins = wylma::warden::parse_instruction(wylma::warden::strings::split_arguments(instructions[i]));
-
     nodes.push_back(n);
   }
 
-  for (const auto &input : inputs) {
+  for (const auto &input : inputs)
+  {
     auto vn = wylma::warden::get_nodes_from_file(input);
     nodes.insert(nodes.end(), vn.begin(), vn.end());
   }
 
-  for (const auto &node : nodes) {
+  for (const auto &node : nodes)
+  {
     wylma::warden::compile_nodes(nodes);
   }
 
